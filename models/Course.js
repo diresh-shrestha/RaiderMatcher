8d023fc00f4384308dcff87d46e86b3644758ac5
Omer571 <53447305+Omer571@users.noreply.github.com>
2020-10-22 17:13:57 -0500

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -1,34 +1,29 @@
 // jshint esversion:8
 const mongoose = require('mongoose');
+const arrayUniquePlugin = require('mongoose-unique-array');
 // const beautifyUnique = require('mongoose-beautiful-unique-validation');
 
 // Course Schema
 const courseSchema = new mongoose.Schema({
-  // department: {
-  //   required: true,
-  //   type: String
-  // },
-  // courseCode: {
-  //   required: true,
-  //   type: Number // This could also be a String
-  // },
-  // _id: mongoose.Schema.Types.ObjectId,
   // "CS 1400"
   course: {
     type: String,
     uppercase: true,
     required: true,
-    unique: 'Course already exists',
+    unique: "Course already exists"
   },
   courseName: {
     type: String,
   },
-  books: [String], // This can make things complicated in coursesUpdateOne()
-  students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }],
+  books: [String],  // This can make things complicated in coursesUpdateOne()
+  students: [{ type: mongoose.Schema.Types.ObjectId , ref: 'Student' }]
 });
 
 // Course Model
 // courseSchema.plugin(beautifyUnique);
+courseSchema.plugin(arrayUniquePlugin);
 const Course = mongoose.model('Course', courseSchema);
 
+
 module.exports = Course;
+
